6f6ffe632b6ed4157b8dc0c46093b1f9f3bbde82
ihasnainbhutta <ihassnainbhutta@gmail.com>
2023-02-28 18:51:43 +0500

    Minor changes

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -16,45 +16,6 @@ function LoginForm() {
   const headers = {
     "Content-Type": "application/json",
   };
-  //   const handleSubmit = async () => {
-  //     try {
-  //       const response = await axios
-  //         .post("http://localhost:8008/user/user-login", {
-  //           email: email,
-  //           password: password,
-  //         })
-  //         .then((response) => {
-  //           if (response.data) {
-  //             alert(response);
-  //           }
-  //           return response.data;
-  //         });
-  //       alert(response);
-  //     } catch (err) {
-  //       alert(err.response.data);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     const res = axios.post(
-  //       "http://localhost:8008/user/user-login",
-  //       { email: email, password: password },
-  //       { headers }
-  //     );
-  //     if (res.status === 200) {
-  //       setCheck(true);
-  //       //   toast.success(res.data);
-  //       //   toast.success(res.data, {
-  //       //     position: toast.POSITION.TOP_RIGHT,
-  //       //   });
-  //       setLoading(false);
-  //       setData(res);
-  //       console.log(res);
-  //     } else {
-  //       setLoading(false);
-  //       alert(res.data.message);
-  //     }
-  //   });
 
   const submit = async (e) => {
     try {
@@ -94,15 +55,6 @@ function LoginForm() {
     } catch (error) {
       console.error(error);
     }
-    //   .then((res) => {
-
-    //   })
-
-    //   .catch((err) => {
-    //     alert("Something Went Wrong!");
-    //     setLoading(false);
-    //     // console.log(err);
-    //   });
   };
   return !check ? (
     <form onSubmit={submit}>
@@ -202,32 +154,5 @@ function LoginForm() {
     </>
   );
 }
-
-// <Form>
-//   <h3>Login</h3>
-
-//   <Form.Group
-//     className="mb-3"
-//     controlId="formBasicEmail"
-//     style={{ marginTop: 30 }}
-//   >
-//     <Form.Label>Email address</Form.Label>
-//     <Form.Control type="email" placeholder="Enter email" />
-//     {/* <Form.Text className="text-muted">
-//       We'll never share your email with anyone else.
-//     </Form.Text> */}
-//   </Form.Group>
-
-//   <Form.Group className="mb-3" controlId="formBasicPassword">
-//     <Form.Label>Password</Form.Label>
-//     <Form.Control type="password" placeholder="Password" />
-//   </Form.Group>
-//   <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//     <Form.Check type="checkbox" label="Remember me" />
-//   </Form.Group>
-//   <Button variant="primary" type="submit">
-//     Submit
-//   </Button>
-// </Form>
 
 export default LoginForm;
--- src/forms/ResetPasswordForm.js
+++ src/forms/ResetPasswordForm.js
@@ -6,58 +6,17 @@ import Spinner from "react-bootstrap/Spinner";
 import { useParams, useNavigate } from "react-router-dom";
 
 function ResetForm() {
-  const [data, setData] = useState("");
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [check, setCheck] = useState(false);
   const [isLoading, setLoading] = useState(false);
-  const [verified, setVerified] = useState(false);
-  console.log(password);
+  // console.log(password);
   const { id } = useParams();
   console.log(id);
   const navigate = useNavigate();
   const headers = {
     "Content-Type": "application/json",
   };
-  //   const handleSubmit = async () => {
-  //     try {
-  //       const response = await axios
-  //         .post("http://localhost:8008/user/user-login", {
-  //           email: email,
-  //           password: password,
-  //         })
-  //         .then((response) => {
-  //           if (response.data) {
-  //             alert(response);
-  //           }
-  //           return response.data;
-  //         });
-  //       alert(response);
-  //     } catch (err) {
-  //       alert(err.response.data);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     const res = axios.post(
-  //       "http://localhost:8008/user/user-login",
-  //       { email: email, password: password },
-  //       { headers }
-  //     );
-  //     if (res.status === 200) {
-  //       setCheck(true);
-  //       //   toast.success(res.data);
-  //       //   toast.success(res.data, {
-  //       //     position: toast.POSITION.TOP_RIGHT,
-  //       //   });
-  //       setLoading(false);
-  //       setData(res);
-  //       console.log(res);
-  //     } else {
-  //       setLoading(false);
-  //       alert(res.data.message);
-  //     }
-  //   });
 
   const submit = async (e) => {
     try {
@@ -72,19 +31,19 @@ function ResetForm() {
         }
       );
       if (res.status === 200) {
+        setCheck(true);
         setLoading(false);
-        setData(res);
         //   toast.success(res.data);
-        navigate("/login");
+        setInterval(navigate("/login"), 2000);
       } else {
         setLoading(false);
-        alert(res.data.message);
+        alert(res);
       }
     } catch (error) {
       console.error(error);
     }
   };
-  return (
+  return !check ? (
     <form onSubmit={submit}>
       <h3>Reset password</h3>
 
@@ -135,34 +94,11 @@ function ResetForm() {
 
       <a href="/login">Return to Login</a>
     </form>
+  ) : (
+    <div>
+      <h3>Thank you! your password has been changed</h3>
+    </div>
   );
 }
-
-// <Form>
-//   <h3>Login</h3>
-
-//   <Form.Group
-//     className="mb-3"
-//     controlId="formBasicEmail"
-//     style={{ marginTop: 30 }}
-//   >
-//     <Form.Label>Email address</Form.Label>
-//     <Form.Control type="email" placeholder="Enter email" />
-//     {/* <Form.Text className="text-muted">
-//       We'll never share your email with anyone else.
-//     </Form.Text> */}
-//   </Form.Group>
-
-//   <Form.Group className="mb-3" controlId="formBasicPassword">
-//     <Form.Label>Password</Form.Label>
-//     <Form.Control type="password" placeholder="Password" />
-//   </Form.Group>
-//   <Form.Group className="mb-3" controlId="formBasicCheckbox">
-//     <Form.Check type="checkbox" label="Remember me" />
-//   </Form.Group>
-//   <Button variant="primary" type="submit">
-//     Submit
-//   </Button>
-// </Form>
 
 export default ResetForm;
